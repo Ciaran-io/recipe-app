df6c5f3f1ee2b134e019cd1551a805eb35929464
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-04-14 00:13:08 +0100

    Deployment to Heroku

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
 // mobile menu
-
 // mobile menu button
 const mobileMenuBtn = document.getElementById('mobile-navigation-btn');
 // mobile header
@@ -11,8 +10,14 @@ const menuIconOpen = document.getElementById('mobile-menu-icon-open');
 // mene icon closed
 const menuIconClosed = document.getElementById('mobile-menu-icon-closed');
 
-// accordion on profile page
+// recipe page
 
+// breakfast menu button
+const buttonMenuBreakfast = document.getElementById('button-menu-breakfast');
+// lunch menu button
+const buttonMenuLunch = document.getElementById('button-menu-lunch');
+// snacks menu buttom
+const buttonMenuSnacks = document.getElementById('button-menu-snacks');
 // accordion list
 const recipeDataDiv = document.querySelectorAll('#accordion-recipe-list');
 // accordion icons
@@ -26,14 +31,7 @@ const breakfastContainer = document.getElementById('breakfast-container');
 const lunchContainer = document.getElementById('lunch-container');
 const snacksContainer = document.getElementById('snacks-container');
 
-// breakfast menu button
-const buttonMenuBreakfast = document.getElementById('button-menu-breakfast');
-// lunch menu button
-const buttonMenuLunch = document.getElementById('button-menu-lunch');
-// snacks menu buttom
-const buttonMenuSnacks = document.getElementById('button-menu-snacks');
-
-// my recipe page
+// users recipe page
 // recipe container first div element
 const userRecipeContainerDiv = document.querySelectorAll(
   '#user-recipe-container > div'
@@ -51,40 +49,50 @@ const buttonExpandImage = document.querySelectorAll('#expand-text-img');
 // close expand text image
 const buttonCloseExpandImage = document.querySelectorAll('#expand-text-close');
 
+// Delete button users recipe
+const buttonDeleteRecipe = document.querySelectorAll('#button-delete-recipe');
+// cancel button users recipe
+const buttonCancelDelete = document.querySelectorAll('#button-cancel-delete');
+// confirm delete button users recipe
+const UserRecipeDeleteContainer = document.querySelectorAll(
+  '#recipe-delete-container'
+);
+
 // mobile menu dropdown
-mobileMenuBtn.addEventListener('click', () => {
+mobileMenuBtn.onclick = () => {
   mobileHeader.classList.toggle('mobile-menu-expand');
   menuIconOpen.classList.toggle('hidden');
   menuIconClosed.classList.toggle('hidden');
   mobileMenu.classList.toggle('h-5/6');
-});
+};
 
-for (let i = 0; i < buttonAccordionDropdown.length; i++) {
-  buttonAccordionDropdown[i].addEventListener('click', () => {
-    recipeData[i].classList.toggle('accordion-recipe-list-expand');
-    buttonAccordionDropdown[i].classList.toggle('accordion-icon-active');
-  });
-}
-
-// my recipe page
+// users recipe page
+// displays hidden recipe info
 for (let i = 0; i < userRecipeContainerUl.length; i++) {
-  buttonExpandText[i].addEventListener('click', () => {
+  buttonExpandText[i].onclick = () => {
     userRecipeContainerUl[i].classList.toggle('recipe-container-expand');
     userRecipeContainerDiv[i].classList.toggle('recipe-container-space');
     fadeSectionDiv[i].classList.toggle('remove-height');
     buttonExpandImage[i].classList.toggle('hidden');
     buttonCloseExpandImage[i].classList.toggle('hidden');
-  });
+  };
+}
+
+for (let i = 0; i < buttonDeleteRecipe.length; i++) {
+  buttonDeleteRecipe[i].onclick = () => {
+    UserRecipeDeleteContainer[i].classList.remove('hidden');
+  };
+}
+
+for (let i = 0; i < buttonCancelDelete.length; i++) {
+  buttonCancelDelete[i].onclick = () => {
+    UserRecipeDeleteContainer[i].classList.add('hidden');
+  };
 }
 
 //recipes page
+// displays hidden recipes from menu
 
-/*
-click function 
-1. adds active class to menu button
-2. displays recipes from clicked button
-3. hides all other recipes & removes active button
-*/
 buttonMenuBreakfast.onclick = () => {
   if (breakfastContainer.classList.contains('hidden')) {
     breakfastContainer.classList.toggle('hidden');
@@ -123,3 +131,11 @@ buttonMenuSnacks.onclick = () => {
   buttonMenuBreakfast.classList.remove('menu-button-active');
   buttonMenuLunch.classList.remove('menu-button-active');
 };
+
+// displays accordion recipe hidden content
+for (let i = 0; i < buttonAccordionDropdown.length; i++) {
+  buttonAccordionDropdown[i].onclick = () => {
+    recipeData[i].classList.toggle('accordion-recipe-list-expand');
+    buttonAccordionDropdown[i].classList.toggle('accordion-icon-active');
+  };
+}
